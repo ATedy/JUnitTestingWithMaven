d603aca4c80bb9922f8d6cb3e109bdb046b3b195
Amanuel <amanuel.tedros@capgemini.com>
2022-03-10 12:26:51 +0000

    BDD terms

--- src/test/java/com/rewards/clip02/RewardByDiscountServiceTest.java
+++ src/test/java/com/rewards/clip02/RewardByDiscountServiceTest.java
@@ -14,9 +14,11 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RewardByDiscountServiceTest {
+    RewardByDiscountService reward = new RewardByDiscountService();
+
     @Test
     void setNeededPoints() {
-        RewardByDiscountService reward = new RewardByDiscountService();
+
         reward.setNeededPoints(100);
 
         assertEquals(100, reward.getNeededPoints());
@@ -25,9 +27,19 @@ class RewardByDiscountServiceTest {
 
    @Test
     void setPercentageForPoints(){
-        RewardByDiscountService reward = new RewardByDiscountService();
         reward.setPercentage(0.1);
 
         assertEquals(0.1, reward.getPercentage());
+   }
+
+   @Test
+    void zeroCustomerPoints() {
+        reward.setPercentage(0.1);
+        reward.setNeededPoints(100);
+
+        Product smallDecaf = new Product(1, "small decaf", 1.99);
+        List<Product> order = Collections.singletonList(smallDecaf);
+
+        RewardInformation info = reward.applyReward(order, 0);
    }
 }
